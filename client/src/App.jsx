6984a6fec0f5462d5c2bde0801779711b156609f
tarunaksha <tarun.garhbeta@gmail.com>
2024-05-30 20:12:50 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Home,
+  Dashboard,
+  About,
+  SignIn,
+  SignUp,
+  Projects,
+} from "./pages/index";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+export { default as Home } from "./Home";
+export { default as Dashboard } from "./Dashboard";
+export { default as About } from "./About";
+export { default as SignIn } from "./SignIn";
+export { default as SignUp } from "./SignUp";
+export { default as Projects } from "./Projects";
+
+// OR THIS WAY
+
+// import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
+// import Projects from "./Projects";
+// import Dashboard from "./Dashboard";
+// import Home from "./Home";
+// import About from "./About";
+
+// export { SignIn, SignUp, Projects, Dashboard, Home, About };

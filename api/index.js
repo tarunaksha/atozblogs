5317ec1ec41691d0c08ca9951381ec806671d0d7
tarunaksha <tarun.garhbeta@gmail.com>
2024-06-01 17:45:38 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_DB_CONNECTION_URL)
@@ -14,6 +18,11 @@ mongoose
   .catch((error) => {
     console.log("Error connecting to MongoDB", error);
   });
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes); 
+app.use("/api/auth", authRoutes);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        uniquw: true,
+        unique: true,
         min: 6,
         max: 24,
     },
